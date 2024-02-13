ffb0c1091c782dc9bd83e04bf358e83fbda273ca
bakafox <v.chekhovskii@g.nsu.ru>
2024-02-13 13:40:33 +0700

    Первые два задания

---
+++ 1 - ping2csv/ping2csv.py
@@ -0,0 +1,49 @@
+import requests
+import pandas as pd
+
+
+def getreason(status_code):
+    codeFirstDigit = status_code // 100
+    if (codeFirstDigit == 1):
+        return 'ℹ️ Information'
+    elif (codeFirstDigit == 2):
+        return '👌 OK!'
+    elif (codeFirstDigit == 3):
+        return '↗️ Non-resolvable Redirect' # "requests" resolves regular redirects automatically, thus the clarification
+    elif (codeFirstDigit == 4):
+        return '💻 Client Error'
+    elif (codeFirstDigit == 5):
+        return '🗄️ Server Error'
+    else:
+        return '❓ (unknown)'
+
+
+domainList = []
+currDomain = ''
+
+while (True):
+    currDomain = input('Enter a domain to add to ping list, or leave empty to start: ')
+    if (currDomain == ''):
+        break
+    domainList.append(currDomain)
+
+
+resultsDF = pd.DataFrame(columns=['Domain', 'Connected?', 'Response (ms)', 'HTTP Code', 'Descrption'])
+
+for i in range(len(domainList)):
+    try:
+        r = requests.get(domainList[i], timeout=3) # connection ok
+        resultsDF.loc[i] = (domainList[i], '✔️ Yes', (r.elapsed.microseconds / 1000), str(r.status_code), getreason(r.status_code))
+    except: # try with http://, just for sure
+        try:
+            r = requests.get('http://' + domainList[i], timeout=3)
+            resultsDF.loc[i] = ('http://' + domainList[i], '✔️ Yes', (r.elapsed.microseconds / 1000), str(r.status_code), getreason(r.status_code))
+        except requests.exceptions.Timeout: # timed out
+            resultsDF.loc[i] = (domainList[i], '⌛ Timed out', '—', '—', '—')
+        except: # connection failed for another reasons
+            resultsDF.loc[i] = (domainList[i], '❌ Failed', '—', '—', '—')
+
+
+output = open('ping2csv.csv', 'w')
+resultsDF.to_csv(output, encoding='utf-8')
+print('\nResults saved to "output.csv".')
---
+++ 2 - ssh & git/recursive-git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# (этот скрипт предполагает, что ssh ключ уже
+# сгенерирован и добавлен в настройках GitHub)
+
+touch "$(date)"
+cd ..
+git add -a
+git commit -m "Тестовый коммит от $(date)"
+git remote add origin git@github.com:bakafox/NSU_ITCN.git
+git push -u origin master
